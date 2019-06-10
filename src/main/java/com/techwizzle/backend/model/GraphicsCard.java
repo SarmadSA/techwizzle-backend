f9ae367603efaba74f4dc139bb32c40c7a15e457
sarmadsa <sarmad@virusoftware.com>
2019-06-10 03:26:50 +0200

    Update GPU model

--- src/main/java/com/techwizzle/backend/model/GraphicsCard.java
+++ src/main/java/com/techwizzle/backend/model/GraphicsCard.java
@@ -1,4 +1,47 @@
 package com.techwizzle.backend.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "GRAPHICS_CARD")
 public class GraphicsCard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String title;
+
+    @Column(name = "RELEASE_PERIOD")
+    private String releasePeriod;
+
+    @Column(name = "AVERAGE_PRICE")
+    private Integer averagePrice;
+
+    private String referralLink;
+
+    private Integer cudaCores;
+
+    private Integer baseClock;
+
+    private Integer boostClock;
+
+    private Integer memoryClockSpeed;
+
+    private String standardMemoryConfig;
+
+    private Integer memoryBandwidth;
+
+    //@ElementCollection
+    //private List<Game> games = new ArrayList<>();
+
 }
