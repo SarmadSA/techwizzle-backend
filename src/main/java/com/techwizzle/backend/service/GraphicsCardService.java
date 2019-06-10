532b4f6888ad766ac279b37b5d29c9e010aac4a1
sarmadsa <sarmad@virusoftware.com>
2019-06-10 03:28:17 +0200

    Implement GPU service

--- src/main/java/com/techwizzle/backend/service/GraphicsCardService.java
+++ src/main/java/com/techwizzle/backend/service/GraphicsCardService.java
@@ -1,4 +1,40 @@
 package com.techwizzle.backend.service;
 
+import com.techwizzle.backend.dao.GraphicsCardDAO;
+import com.techwizzle.backend.model.GraphicsCard;
+
+import java.util.List;
+
 public class GraphicsCardService {
+
+    private GraphicsCardDAO graphicsCardDAO;
+
+    public GraphicsCardService() {
+        this.graphicsCardDAO = new GraphicsCardDAO();
+    }
+
+    public List<GraphicsCard> getAllGraphicsCards(){
+        try{
+            graphicsCardDAO.openCurrentSession();
+            return graphicsCardDAO.findAll();
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            graphicsCardDAO.closeCurrentSession();
+        }
+        return null;
+    }
+
+    public GraphicsCard addGraphicsCard(GraphicsCard graphicsCard){
+        try{
+            graphicsCardDAO.openCurrentSessionwithTransaction();
+            graphicsCardDAO.persist(graphicsCard);
+            return graphicsCard;
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            graphicsCardDAO.closeCurrentSessionwithTransaction();
+        }
+        return null;
+    }
 }
