0b0915d450a7c8bfa9e7df768a75779f01e3e5a7
sarmadsa <sarmad@virusoftware.com>
2019-06-10 03:29:03 +0200

    Add a graphics card resource with simple GET & POST methods

--- src/main/java/com/techwizzle/backend/resource/GraphicsCardResource.java
+++ src/main/java/com/techwizzle/backend/resource/GraphicsCardResource.java
@@ -1,4 +1,29 @@
 package com.techwizzle.backend.resource;
 
+
+import com.techwizzle.backend.model.GraphicsCard;
+import com.techwizzle.backend.service.GraphicsCardService;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/graphicsCards")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class GraphicsCardResource {
+
+    //@Inject
+    private GraphicsCardService graphicsCardService = new GraphicsCardService();
+
+    @GET
+    public List<GraphicsCard> listAllGraphicsCards(){
+        return graphicsCardService.getAllGraphicsCards();
+    }
+
+    @POST
+    public GraphicsCard addGraphicsCard(GraphicsCard graphicsCard){
+        return graphicsCardService.addGraphicsCard(graphicsCard);
+    }
+
 }
